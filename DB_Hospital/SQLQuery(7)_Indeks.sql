560ea190b10785fbf4c84dafe58cc060b3d39a6a
Laika <110253177+wiktoriamaciasz@users.noreply.github.com>
2023-01-30 12:23:25 +0100

    Update SQLQuery(7)_Indeks.sql

--- DB_Hospital/SQLQuery(7)_Indeks.sql
+++ DB_Hospital/SQLQuery(7)_Indeks.sql
@@ -7,7 +7,7 @@ CREATE INDEX IX_Pacjenci_Nazwisko
 ON Pacjenci (Nazwisko); 
 
 
-CREATE INDEX IX_Pacjenci_Nazwisko
+CREATE INDEX IX_Pacjenci_Nazwisko2
 ON Pacjenci (Nazwisko)
 INCLUDE (Imie)
 WHERE Palenie=1
