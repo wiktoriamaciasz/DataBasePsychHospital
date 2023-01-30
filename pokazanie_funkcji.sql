d2f3bffd036693f0c70e1a084fc3b0c370ff7b3f
PiotrRyw <72543189+PiotrRyw@users.noreply.github.com>
2023-01-30 09:33:04 +0100

    Update pokazanie_funkcji.sql
    wyzwalacz INSTEAD OF DELETE AktualneStrategieLeczenia
    zmiana w funkcjach vw_Pacjenci_Lekarzy_AKT, vw_Pacjenci_Lekarzy

--- pokazanie_funkcji.sql
+++ pokazanie_funkcji.sql
@@ -1,31 +1,35 @@
---FUNKCJA WYLICZAJ•CA WIEK PACJENTA
+--FUNKCJA WYLICZAJƒÑCA WIEK PACJENTA
 
 SELECT ObliczWiek(2)
 
--- WIDOK PODAJ•CY INFORMACJE O DNIACH POBYTU W SZPITALU I LICZBIE HOSPITALIZACJI
+-- WIDOK PODAJƒÑCY INFORMACJE O DNIACH POBYTU W SZPITALU I LICZBIE HOSPITALIZACJI
 
 SELECT * FROM Info_Pobyty
 
--- WIDOK INFORMUJ•CY O WYPE£NIENIU ODDZIAL”W SZPITALNYCH PRZEZ PACJENT”W
+-- WIDOK INFORMUJƒÑCY O WYPE≈ÅNIENIU ODDZIAL√ìW SZPITALNYCH PRZEZ PACJENT√ìW
 SELECT * FROM DostepneMiejscaNaOddzialach
 
--- WIDOK pokazujπcy lekarzy i ich obecnych pacjentow
-SELECT * FROM vw_Pacjenci_Lekarzy 
 
--- FUNKCJA pokazujπca lekarza i jego obecnych pacjentÛw
-SELECT * FROM f_Pacjenci_Lekarza('Milena', 'Krajewska')
+-- WIDOK pokazujƒÖcy lekarzy i ich wszystkich pacjentow
+SELECT * FROM vw_Pacjenci_Lekarzy
 
--- FUNKCJA pokazujπca lekarza i jego by≥ych pacjentÛw
-SELECT * FROM f_Historia_PacjentÛw_Lekarza('Milena', 'Krajewska')
+-- WIDOK pokazujƒÖcy lekarzy i ich obecnych pacjentow
+SELECT * FROM vw_Pacjenci_Lekarzy_AKT
 
--- FUNKCJA pokazujπca pacjenta i jego obecnych lekarzy
-SELECT * FROM f_Lekarze_Pacjenta('Zofia', 'Janik')
+-- FUNKCJA pokazujƒÖca lekarza i jego obecnych pacjent√≥w
+SELECT * FROM f_Pacjenci_Lekarza('Patrycja', 'Nowakowska')
 
--- FUNKCJA pokazujπca pacjenta i jego by≥ych lekarzy
-SELECT * FROM f_Historia_Lekarzy_Pacjenta('Zofia', 'Janik')
+-- FUNKCJA pokazujƒÖca lekarza i jego by≈Çych pacjent√≥w
+SELECT * FROM f_Historia_Pacjent√≥w_Lekarza('Patrycja', 'Nowakowska')
+
+-- FUNKCJA pokazujƒÖca pacjenta i jego obecnych lekarzy
+SELECT * FROM f_Lekarze_Pacjenta('Monika', 'Sobczak')
+
+-- FUNKCJA pokazujƒÖca pacjenta i jego by≈Çych lekarzy
+SELECT * FROM f_Historia_Lekarzy_Pacjenta('Monika', 'Sobczak')
 	
 
---Widok zwracajπcy czÍstoúÊ wystÍpowania danych jednostek chorobowych w szpitalu
+--Widok zwracajƒÖcy czƒôsto≈õƒá wystƒôpowania danych jednostek chorobowych w szpitalu
 SELECT * FROM CzestoscWystepowaniaJednostekChoorbowych
 
 
@@ -33,66 +37,80 @@ SELECT * FROM CzestoscWystepowaniaJednostekChoorbowych
 -------Wyzwalacze i Procedury----------
 ---------------------------------------
 
--- w tabeli pacjenci jest kolumna dataUrodzenia i Pesel na wypadek pacjentÛw nieposiadajπcych numeru pesel i/lub udokumentowanej daty urodzenia
+-- w tabeli pacjenci jest kolumna dataUrodzenia i Pesel na wypadek pacjent√≥w nieposiadajƒÖcych numeru pesel i/lub udokumentowanej daty urodzenia
 
 -- WYZWALACZ INSTEAD OF INSERT
 -- NA TABELI PACJENCI
--- SPRAWDZAJ•CY DODAWANE DANE DO TABELI PACJENCI- POPRAWNOå∆/ZGODNOå∆ PESELA I DATY
+-- SPRAWDZAJƒÑCY DODAWANE DANE DO TABELI PACJENCI- POPRAWNO≈öƒÜ/ZGODNO≈öƒÜ PESELA I DATY
 
 INSERT INTO Pacjenci VALUES
-('Tymoteusz', 'JasiÒski', 'M', '1972-05-04', '72050459950', 1, 178),
-('Dominik', 'KamiÒski', 'M', '1988-05-19', '88051960935', 0, 185),
-('Lidia', 'Kaczmarek', 'K', '1928-05-12', '29051210920', 1, 184), -- z≥y pesel, dobry: 28051210920
-('Norbert', 'Paw≥owski', 'M', '1949-02-13', '49021367890', 1, 179), -- z≥y pesel, dobry: 49021367891
+('Tymoteusz', 'Jasi≈Ñski', 'M', '1972-05-04', '72050459950', 1, 178),
+('Dominik', 'Kami≈Ñski', 'M', '1988-05-19', '88051960935', 0, 185),
+('Lidia', 'Kaczmarek', 'K', '1928-05-12', '29051210920', 1, 184), -- z≈Çy pesel, dobry: 28051210920
+('Norbert', 'Paw≈Çowski', 'M', '1949-02-13', '49021367890', 1, 179), -- z≈Çy pesel, dobry: 49021367891
 ('Zygmunt', 'Sobczak', 'M', '2005-06-26', '05262649138', 1, 171),
-('Wioletta', 'Jab≥oÒska', 'K', '1953-11-18', NULL, 0, 187)
+('Wioletta', 'Jab≈Ço≈Ñska', 'K', '1953-11-18', NULL, 0, 187)
 
 
 -- WYZWALACZ INSTEAD OF UPDATE
 -- NA TABELI PACJENCI
--- SPRAWDZAJ•CY MODYFIKOWANE DANE DO TABELI PACJENCI- POPRAWNOå∆/ZGODNOå∆ PESELA I DATY
+-- SPRAWDZAJƒÑCY MODYFIKOWANE DANE DO TABELI PACJENCI- POPRAWNO≈öƒÜ/ZGODNO≈öƒÜ PESELA I DATY
 UPDATE Pacjenci
 SET Pesel = '58032157277'
-WHERE Pesel = '58032157278' -- nie przejdzie, bo by≥by b≥Ídny pesel
+WHERE Pesel = '58032157278' -- nie przejdzie, bo by≈Çby b≈Çƒôdny pesel
 
 SELECT * FROm Pacjenci
 WHERE Pesel = '58032157278'
 
 UPDATE Pacjenci
 SET DataUrodzenia = '1958-04-21'
-WHERE Pesel = '58032157278' -- nie przejdzie, bo by≥by b≥Ídny pesel
+WHERE Pesel = '58032157278' -- nie przejdzie, bo by≈Çby b≈Çƒôdny pesel
 
 UPDATE Pacjenci
 SET DataUrodzenia = '1958-04-21'
-WHERE Pesel = '58042157278' -- przejdzie, zmiana jest spÛjna
+WHERE Pesel = '58042157278' -- przejdzie, zmiana jest sp√≥jna
 
 UPDATE Pacjenci
 SET Nazwisko = 'Nowak'
-WHERE Pesel = '76121257869' -- przejdzie, zmiana jest spÛjna
+WHERE Pesel = '76121257869' -- przejdzie, zmiana jest sp√≥jna
 
 
 -- WYZWALACZ INSTEAD OF INSERT
 -- NA TABELI PRACOWNICY
--- SPRAWDZAJ•CY MODYFIKOWANE DANE DO TABELI PACJENCI- POPRAWNOå∆ PESELA
+-- SPRAWDZAJƒÑCY MODYFIKOWANE DANE DO TABELI PACJENCI- POPRAWNO≈öƒÜ PESELA
 INSERT INTO Pracownicy VALUES
-('Wies≥aw', 'Laskowski', 'M', '29083059734', 'PielÍgniarz', 'Magister', 4886, 0, '1958-05-11', '+48 721675648', 'ul. Karminowa 136/29', 'SÍdziszÛw Ma≥opolski'),
-('Tadeusz', 'Krawczyk', 'M', '44081854155', 'Lekarz', 'Doktor Habilitowany', 16752, 0, '1991-02-19', '+48 694041151', 'ul. Wroc≥awska 96', 'Be≥øyce'), -- z≥y pesel, dobry 44081854150
-('Milena', 'Pawlak', 'K', NULL, 'Lekarz', 'Doktor Habilitowany', 16752, 0, '2003-03-26', '+48 722240867', 'ul. Zacisze 41/39', 'MakÛw PodhalaÒski')
+('Wies≈Çaw', 'Laskowski', 'M', '29083059734', 'Pielƒôgniarz', 'Magister', 4886, 0, '1958-05-11', '+48 721675648', 'ul. Karminowa 136/29', 'Sƒôdzisz√≥w Ma≈Çopolski'),
+('Tadeusz', 'Krawczyk', 'M', '44081854155', 'Lekarz', 'Doktor Habilitowany', 16752, 0, '1991-02-19', '+48 694041151', 'ul. Wroc≈Çawska 96', 'Be≈Ç≈ºyce'), -- z≈Çy pesel, dobry 44081854150
+('Milena', 'Pawlak', 'K', NULL, 'Lekarz', 'Doktor Habilitowany', 16752, 0, '2003-03-26', '+48 722240867', 'ul. Zacisze 41/39', 'Mak√≥w Podhala≈Ñski')
 
 INSERT INTO Pracownicy VALUES
-('Janina', 'Maciejewska', 'K', '72052539942', 'Diagnostyk Laboratoryjny', 'Magister', 5863, 0, '2010-10-24', '+48 667925799', 'ul. PartyzantÛw 43/21', 'Sochaczew'),
-('Janina', 'Maciejewska', 'K', '72052539942', 'Diagnostyk Laboratoryjny', 'Magister', 5863, 0, '2010-10-24', '+48 667925799', 'ul. PartyzantÛw 43/21', 'Sochaczew') -- nie przejdzie, ten sam PESEL
+('Janina', 'Maciejewska', 'K', '72052539942', 'Diagnostyk Laboratoryjny', 'Magister', 5863, 0, '2010-10-24', '+48 667925799', 'ul. Partyzant√≥w 43/21', 'Sochaczew'),
+('Janina', 'Maciejewska', 'K', '72052539942', 'Diagnostyk Laboratoryjny', 'Magister', 5863, 0, '2010-10-24', '+48 667925799', 'ul. Partyzant√≥w 43/21', 'Sochaczew') -- nie przejdzie, ten sam PESEL
 
 -- WYZWALACZ INSTEAD OF UPDATE
 -- NA TABELI PRACOWNICY
--- SPRAWDZAJ•CY MODYFIKOWANE DANE DO TABELI PACJENCI- POPRAWNOå∆ PESELA
+-- SPRAWDZAJƒÑCY MODYFIKOWANE DANE DO TABELI PACJENCI- POPRAWNO≈öƒÜ PESELA
 
 UPDATE Pracownicy
 SET Nazwisko = 'Nowak'
-WHERE Pesel = '44081854155' -- przejdzie, zmiana jest spÛjna
+WHERE Pesel = '44081854155' -- przejdzie, zmiana jest sp√≥jna
+
+-- WYZWALACZ INSTEAD OF DELETE
+-- NA TABELI AktualneStrategie Leczenia
+SELECT * FROM AktualneStrategieLeczenia
+
+SELECT * FROM HistoriaStrategieLeczenia
+
+DELETE FROM AktualneStrategieLeczenia
+WHERE ID = 6
+
+SELECT * FROM AktualneStrategieLeczenia
+
+SELECT * FROM HistoriaStrategieLeczenia
+
 
 /*
-Procedura dodajπca strategiÍ leczenia dla danego pacjenta (imie, nazwisko, pesel) przypisujπc mu lekarza
+Procedura dodajƒÖca strategiƒô leczenia dla danego pacjenta (imie, nazwisko, pesel) przypisujƒÖc mu lekarza
 */
 EXEC DodajLeczenie
 	@imie_pacj = 'Mariola',
@@ -103,20 +121,20 @@ EXEC DodajLeczenie
 	@pesel_lek = '82030517264'
 
 /*
-Procedura wpisujπca detale leczenia
+Procedura wpisujƒÖca detale leczenia
 */
 EXEC UzupelnijLeczenie
 	@id_leczenia = 7,
-	@choroba = 'Zaburzenie osobowoúci',
+	@choroba = 'Zaburzenie osobowo≈õci',
 	@lek = 'Olzapin',
 	@dawka = 10,
-	@oddzial = 'Oddzia≥ OgÛlnopsychiatryczny'
+	@oddzial = 'Oddzia≈Ç Og√≥lnopsychiatryczny'
 
 
 GO
 
 /*
-Procedura wpisujπca datÍ koÒcowπ leczenia
+Procedura wpisujƒÖca datƒô ko≈ÑcowƒÖ leczenia
 */
 EXEC ZakonczLeczenie
 	@id_leczenia = 4
